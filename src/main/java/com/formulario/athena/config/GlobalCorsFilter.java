e504b3b01ffe0c63382784dd050de8e95e9ec3e7
GabrielAthenaOffice <gabrielathenaoffice@gmail.com>
2025-10-08 13:55:59 -0300

    aaaai vai

--- src/main/java/com/formulario/athena/config/GlobalCorsFilter.java
+++ src/main/java/com/formulario/athena/config/GlobalCorsFilter.java
@@ -2,13 +2,8 @@ package com.formulario.athena.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 public class GlobalCorsFilter {
@@ -16,7 +11,7 @@ public class GlobalCorsFilter {
 
     @Bean
     public FilterRegistrationBean<org.springframework.web.filter.CorsFilter> corsFilter() {
-        var cfg = new org.springframework.web.cors.CorsConfiguration();
+        var cfg = new CorsConfiguration();
         cfg.setAllowCredentials(false);
         cfg.setAllowedOriginPatterns(java.util.List.of(
                 "https://front-correspondencias-athena.vercel.app",
